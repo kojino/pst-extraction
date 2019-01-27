333e1d203fdffd7cf24b10a34a517d3349c58ea0
Kojin Oshiba <kojin.oshiba@gmail.com>
2019-01-26 22:02:34 -0500

    add sentence embedding shell

---
+++ bin/run_spark_embedding.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "===========================================$0 $@"
+
+START=$(date +%s)
+
+OUTPUT_DIR=spark-emails-embedding
+if [[ -d "pst-extract/$OUTPUT_DIR" ]]; then
+    rm -rf "pst-extract/$OUTPUT_DIR"
+fi
+
+spark-submit --master local[*] --driver-memory 16g --conf spark.storage.memoryFraction=.8 --files spark/filters.py spark/embedding.py --input_path pst-extract/spark-emails-text-rmhtml --output_path pst-extract/$OUTPUT_DIR
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "It took $DIFF seconds"
+#./bin/validate_lfs.sh $OUTPUT_DIR
